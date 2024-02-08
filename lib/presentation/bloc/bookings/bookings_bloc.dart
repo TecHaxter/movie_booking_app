73ac1a77c02971ae9630d0795190fc1a428c517d
Khushal Singh Rao <mr.khushalrao@gmail.com>
2024-02-09 02:24:03 +0530

    Booking bloc empty state fix

--- lib/presentation/bloc/bookings/bookings_bloc.dart
+++ lib/presentation/bloc/bookings/bookings_bloc.dart
@@ -7,26 +7,26 @@ class BookingsBloc extends Bloc<BookingsEvent, BookingsState> {
   final FetchMyBookingsUsecase fetchMyBookingsUsecase;
 
   BookingsBloc(this.fetchMyBookingsUsecase)
-      : super(BookingsEmpty(
-          'Wait while we fetch more Bookings for the movies',
-        )) {
+      : super(BookingsEmpty('No Bookings Found')) {
     on<OnFetchBookings>((event, emit) async {
       emit(BookingsLoading(
         'Wait while we fetch your Bookings',
       ));
       final res = await fetchMyBookingsUsecase.call();
       final data = res.fold((l) => null, (r) => r);
-      BookingsState? errorState;
-      if (res.isLeft() || data == null) {
-        errorState = BookingsFailed(
+      if (res.isLeft()) {
+        emit(BookingsFailed(
           'Error occurred while fetching Bookings',
-        );
+        ));
+        return;
       }
-      if (errorState != null) {
-        emit(errorState);
-        await Future.delayed(const Duration(seconds: 2));
+      if (data == null) {
+        emit(BookingsEmpty(
+          'No Bookings Found',
+        ));
+        return;
       }
-      emit(BookingsLoaded(data!));
+      emit(BookingsLoaded(data));
     });
   }
 }
